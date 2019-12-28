8a96201a12cfd482fd2f3b097d4e5fad20698185
Marcin <purpurovvy@gmail.com>
2019-12-29 00:07:33 +0100

    login and register templates

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/login";
+import SignUp from "./components/signup";
+import Home from "./components/home";
 
 function App() {
-  return (
+  return (<Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Test tekst.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/"}>CodersCamp Cinema</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      
+      <div className="cinemaContainer">
+      
+        <div className="authorizationPanel">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
 }
 
---
+++ client/src/components/home.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+    render() {
+        return (
+         <div> <h3>Hello there in best cinema ever !! :)</h3> 
+         </div>
+        );
+    }
+}
